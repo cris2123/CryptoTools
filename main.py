496d4b15db81d225d132d28502c258108f51d4a5
cristhian <cristhian9bravo@gmail.com>
2018-03-06 19:33:17 -0400

    Add support for cli arguments

--- main.py
+++ main.py
@@ -26,13 +26,8 @@ class coinMarket:
         self.coinNames={}
         self.wholeContent=""
 
-        self.__debugGetCoinNames()
-
-        print(self.wholeContent)
-
-
         ## funcion para obteenr todos las monedas en coin market
-        #self.getCoinNames()
+        self.getCoinNames()
 
 
     def _checkValidFiat(self,fiat):
@@ -43,7 +38,7 @@ class coinMarket:
 
         try:
 
-            if(fiat!=""):
+            if(fiat!="" or fiat==None):
                 if(type(fiat)!=str):
                     print("Fiat invalid type")
                     raise FiatInvalidType
@@ -89,21 +84,6 @@ class coinMarket:
             isValidCoin=True
 
         return(isValidCoin)
-
-    def __debugGetCoinNames(self):
-
-        # with open("currencyData.json",'r') as jsonFile:
-        #     allData= json.load(jsonFile)
-
-
-        if(allData):
-
-            for data in allData:
-
-                self.coinNames[data["name"]]=(data["symbol"],data["id"])
-
-        self.wholeContent=allData
-
 
     def getCoinNames(self):
 
@@ -224,27 +204,31 @@ class coinMarket:
                     arrayValidCoins.append(tuple(results))
 
 
+        print(arrayValidCoins)
         currencyFiat=self._checkValidFiat(fiat)
 
-        print(arrayValidCoins)
+
+        print(currencyFiat)
 
         coinInformation=[]
 
-        with open("global.json",'w') as jsonFile:
-            jsonFile.write(str(data))
+        # with open("global.json",'w') as jsonFile:
+        #     jsonFile.write(str(data))
+
 
         for item in self.wholeContent:
 
             for tupleCoin in arrayValidCoins:
-                print("Coin:"+str(tupleCoin[0]))
+
 
                 if(tupleCoin[1]==True):
-
+                    print("Llegue a tupleCoin[1]:")
                     if(item["id"]==tupleCoin[0]):
-
+                        print("Tuple coin es:"+ str(tupleCoin[0]))
                         coinInformation.append(item)
+
                 else:
-                    coinInformation.append("coin: "+tupleCoin[0]+" is not a valid one")
+                    coinInformation.append("coin: "+ tupleCoin[0]+" is not a valid one")
 
             if(len(coinInformation)==len(coins)):
                 break;
@@ -337,12 +321,26 @@ class coinMarket:
 
 if __name__=="__main__":
 
+
+    #### Parser para la linea de comandos
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--coin", type=str,
+	help="search for data of a coin by name or symbol on coinMarket")
+
+    parser.add_argument("-cl","--coin-lists",type=list,
+    help="search for data of a list of coins by name or symbol on coinMarket")
+
+    parser.add_argument("-f","--fiat",type=str,help="get data of a coin in alternative currency")
+
+    args = vars(parser.parse_args())
+
+    #### Clase para el API de CoinMarket
     Market=coinMarket()
 
-    # # #Market.connect()
-    #Market.getCoin(coin="SBD",fiat="EUR")
+    #Market.getCoin(coin=args["coin"],fiat=args["fiat"])
     #Market.getAllCoins(fiat=2)
-    #Market.getGlobalData(fiat=2)
-    #coins=["Bitcoin","Steem Dollars","Ethereum","CRIS"]
+    #Market.getGlobalData(fiat=args["fiat"])
+    coins=["Bitcoin","Steem Dollars","Ethereum","CRIS"]
     # print(type(coins))
-    #Market.getListCoins(coins,fiat="")
+    Market.getListCoins(coins,fiat="")
